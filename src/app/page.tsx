ccd7b04bf188d8d2aa06e05b272fdb58e5e19fae
faYassoy <pokyjand01@gmail.com>
2024-11-18 11:11:37 +0700

    scafhold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
 
 export default function HomePage() {
+  const mockUrl =[
+    'https://utfs.io/f/URKYdZLRQnfki3xZtkuJ80NTvLEqZDXKHGcsgMj6eUCxSlAr',
+    'https://utfs.io/f/URKYdZLRQnfkk6fiivCETF3PMDUEJAi0Spg8h2NuwoqXvjtR',
+    'https://utfs.io/f/URKYdZLRQnfkCBOI9z3dHPwNmx95yY8gocTzApeQSEtrFXIs',
+    'https://utfs.io/f/URKYdZLRQnfkNjgiv83UybvVxT5CaPg0DGkS3oEfr41OeFIt'
+  ]
+  const images = [...mockUrl,...mockUrl,...mockUrl].map((url,index)=> ({id:index+1, url}))
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      inprogress
+    <main className="">
+      <div className="flex flex-wrap gap-4 justify-center lg:justify-normal">
+        {images.map((img)=> <div key={img.id} className="w-48"><img src={img.url} /></div>
+      )}
+      </div>
     </main>
   );
 }
